5f574ad8160d3d4f294665f2ff0301382b2da69a
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2024-01-18 11:51:56 -0500

    Connected to mongodb and updated routes name

--- backend/routes/travelExperiences.js
+++ backend/routes/travelExperiences.js
@@ -4,27 +4,27 @@ const router = express.Router();
 
 // GET all
 router.get('/', (req, res) => {
-    res.json({msg: 'GET all testRoutes'})
+    res.json({msg: 'GET all travel experiences'})
 });
 
 // GET one
 router.get('/:id', (req, res) => {
-    res.json({msg: 'GET one testRoute'})
+    res.json({msg: 'GET one travel experience'})
 })
 
 // POST a new one
 router.post('/', (req, res) => {
-    res.json({msg: 'POST a new testRoute'})
+    res.json({msg: 'POST a new travel experience'})
 })
 
 // DELETE one
 router.delete('/:id', (req, res) => {
-    res.json({msg: 'DELETE a testRoute'})
+    res.json({msg: 'DELETE a travel experience'})
 })
 
 // UPDATE one
 router.patch('/:id', (req, res) => {
-    res.json({msg: 'UPDATE a testRoute'})
+    res.json({msg: 'UPDATE a travel experience'})
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
 require('dotenv').config();
 
 const express = require('express');
-const testRoutes = require('./routes/testRoutes');
+const mongoose = require('mongoose');
+const testRoutes = require('./routes/travelExperiences');
 
 const app = express();
 
-app.use(express.json())
-
+// middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
+// routes
 app.use('/api/testRoutes', testRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log('Listening on port 4000');
-});
+// connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to DB & listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });

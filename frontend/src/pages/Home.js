aaa9fe7faf80dd138c144b0af5535fb512279317
amethystcrystal <crystalbardowell>
2024-01-20 12:00:42 -0500

    .

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react';
+import Experiences from '../components/Experiences';
+
 const Home = () => {
+  const [experiences, setExperiences] = useState(null);
+
+  useEffect(() => {
+    const fetchExperiences = async () => {
+      const response = await fetch('/api/TravelExperiences');
+      const json = await response.json();
+      if (response.ok) {
+        setExperiences(json);
+      }
+    };
+    fetchExperiences();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="experiences">
+        {experiences &&
+          experiences.map((experience) => (
+           <Experiences key={experience._id} experience={experience}/>
+          ))}
+      </div>
     </div>
   );
 };

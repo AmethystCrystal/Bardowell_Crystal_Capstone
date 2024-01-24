2ddf6888f85d39128ed260c97e965fb0fe3249da
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2024-01-24 14:34:35 -0500

    date-fns package to format date of submissions

--- frontend/src/components/Experiences.js
+++ frontend/src/components/Experiences.js
@@ -1,10 +1,12 @@
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const Experiences = ({ experience }) => {
   return (
     <div className="experience-details">
       <p style={{ fontSize: '16px' }}><strong>Name:</strong> {experience.name}</p>
       <p style={{ fontSize: '16px' }}><strong>Title:</strong> {experience.title}</p>
       <p style={{ fontSize: '20px' }}>{experience.body}</p>
-        <p>{experience.createdAt}</p>
+        <p>{formatDistanceToNow(new Date(experience.createdAt), { addSuffix: true })}</p>
     </div>
   );
 };
